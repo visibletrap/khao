5ed5cb4959d0e91ffb6c0f4327f386b4b9919c8e
Tap <visibletrap@gmail.com>
2016-09-10 01:29:45 +0800

    Basic restaurant recommendation

--- src/khao/core.clj
+++ src/khao/core.clj
@@ -18,7 +18,18 @@
              (let [next-id (inc (or (max-of :id eats) 0))]
                (conj eats {:id next-id :restaurant rkey :ate-at ate-at})))))
 
+(defn former-eats [eats]
+  (sort-by :ate-at eats))
+
+(defn format-eat [{:keys [restaurant ate-at]} restaurants]
+  (let [{:keys [name]} (get restaurants restaurant)]
+    (str name ":         " ate-at)))
+
+(defn recommend-restaurants [{:keys [eats restaurants]}]
+  (map #(format-eat % restaurants) (former-eats eats)))
+
 (comment
   @S
   (add-restaurant S :shop "KFC" :western/fast-food)
-  (add-eat S :shop #inst "2016-09-05T19:40"))
+  (add-eat S :shop #inst "2016-09-05T19:40")
+  (recommend-restaurants @S))
